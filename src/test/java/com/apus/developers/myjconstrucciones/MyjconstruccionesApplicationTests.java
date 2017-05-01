3a971668ee173ae6390b19bd4ceaad45f5c67bcb
leebeth <lorena_2328@hotmail.com>
2017-05-01 12:27:25 -0500

    Commit Inicial - Hello World

---
+++ src/test/java/com/apus/developers/myjconstrucciones/MyjconstruccionesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.apus.developers.myjconstrucciones;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyjconstruccionesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
